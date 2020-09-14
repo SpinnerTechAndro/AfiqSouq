6ae197c08432c596d96ed26c43dab348a636758a
nmkawcher <nmkawcher112@gmail.com>
2020-09-14 23:10:22 +0600

    division one  all layout designed

---
+++ app/src/main/java/dev/spinner_tech/afiqsouq/CheckoutActivity.java
@@ -0,0 +1,14 @@
+package dev.spinner_tech.afiqsouq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+    }
+}
