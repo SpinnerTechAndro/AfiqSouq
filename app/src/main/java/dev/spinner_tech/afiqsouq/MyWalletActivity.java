531a4c1dbbf7862f0a3ad27007c85058afcda9fd
NM Kawcher <46804139+nmkawcher@users.noreply.github.com>
2020-10-27 11:51:28 +0600

    top up account balance layout is created and rewards layout is updated

---
+++ app/src/main/java/dev/spinner_tech/afiqsouq/MyWalletActivity.java
@@ -0,0 +1,14 @@
+package dev.spinner_tech.afiqsouq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyWalletActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_wallet);
+    }
+}
---
+++ app/src/main/java/dev/spinner_tech/afiqsouq/TopUpAccountBalanceActivity.java
@@ -0,0 +1,14 @@
+package dev.spinner_tech.afiqsouq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopUpAccountBalanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_up_account_balance);
+    }
+}
